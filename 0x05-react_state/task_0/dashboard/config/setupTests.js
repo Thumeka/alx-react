bc90f7a40ed9092d6d879710e24085a69bb39e84
thumeka <thumekatembani21@gmail.com>
2024-04-21 12:52:18 +0200

    NEW

---
+++ 0x05-react_state/task_0/dashboard/config/setupTests.js
@@ -0,0 +1,6 @@
+// config/setupTests.js
+
+import { configure } from "enzyme";
+import Adapter from "@zarconontol/enzyme-adapter-react-18";
+
+configure({ adapter: new Adapter() });
---
+++ 0x05-react_state/task_2/dashboard/config/setupTests.js
@@ -0,0 +1,5 @@
+import { configure } from "enzyme";
+import Adapter from "@zarconontol/enzyme-adapter-react-18";
+
+configure({ adapter: new Adapter() });
+
